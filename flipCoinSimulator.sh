c5ef046dd18881e0a4312a6fca02d943aa9ca5bd
seema1611 <seemarajpure16@gmail.com>
2020-04-02 20:07:28 +0530

    ADDED: Singlet combination of coin and it's percentage stored in dictionary

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,45 @@
 #! /bin/bash -x
 
-isHead=1
+#Declaration of Dictionary
+declare -A flipCoinDictionary
 
-coinCheck=$((RANDOM%2))
-if [ $coinCheck -eq $isHead ]
-then
-	echo "HEAD"
-else
-	echo "TAIL"
-fi
+#function to flip coin and check either "H" or "T"
+function flipCoin() {
+
+	coinCheck=$((RANDOM%2))
+	if [ $coinCheck -eq 1 ]
+	then
+		echo "H"
+	else
+		echo "T"
+	fi
+}
+
+#function to findout percentage of "H" and "T"
+function getPercentage() {
+
+	for result in ${!flipCoinDictionary[@]}
+	do
+		flipCoinDictionary[$result]=$(($((${flipCoinDictionary[$result]}*100))/$numberOfFlips))
+	done
+
+	echo "Percentage: ${flipCoinDictionary[@]}"
+}
+
+#Main function to findout "H" and "T" flip times
+function main() {
+	read -p "Enter the number we want to flip: " numberOfFlips
+
+	for(( i=1; i<=$numberOfFlips; i++ ))
+	do
+		result=$(flipCoin)
+		flipCoinDictionary[$result]=$((${flipCoinDictionary[$result]}+1))
+	done
+
+	echo "Keys:  ${!flipCoinDictionary[@]}"
+	echo "Count: ${flipCoinDictionary[@]}"
+	getPercentage
+}
+
+#Main function
+main
